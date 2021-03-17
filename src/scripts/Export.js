a83d03844d43bc584fdcde764799a519edd83d8e
Adam Morris <amorris@mistermorris.com>
2021-03-17 16:51:33 +0800

    behavior notes

--- src/scripts/Export.js
+++ src/scripts/Export.js
@@ -42,3 +42,25 @@ function manageBacV2AuthToken ({token, count=30}={},
   saveToSpreadsheet_({id, sheetName, jsons, numHeaderRows,
                       priorityHeaders: ['id', 'student_id', 'first_name', 'last_name', 'class_grade', 'email']});
 }
+
+/**
+ * Download OpenApply via Bearer token
+ * @param {Object} settings
+ * @param {String} settings.token - The bearer token from the managebac settings
+ * @param {Number} [settings.count=100] - How many to download at a time
+ * @param {Object} ss
+ * @param {String} ss.id - The id of the target spreadsheet to write to
+ * @param {String} [ss.sheetName='Sheet1'] - The name of the sheet
+ */
+function manageBacV2AuthTokenBehaviour ({token, count=100}={},
+                          {id, sheetName}={},
+                          {numHeaderRows=1}={})
+{
+  const subdomain = 'api';
+  const domain = 'managebac.com';
+  const version = 'v2';
+  const endpoint = initWithAuthToken_({token, subdomain, domain, version, count});
+  const jsons = downloadMBBehaviourv2_(endpoint);
+  saveToSpreadsheet_({id, sheetName, jsons, numHeaderRows,
+                      priorityHeaders: ['student_id', 'email', 'first_name', 'last_name', 'reported_by', 'notes']});
+}
